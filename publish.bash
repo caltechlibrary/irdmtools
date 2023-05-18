c12046e1ef72acfaed295d63ff323602d86e08a0
R. S. Doiel <rsdoiel@gmail.com>
2023-05-18 09:40:54 -0700

    Quick Save

--- publish.bash
+++ publish.bash
@@ -15,17 +15,15 @@ else
 		git push origin "$WORKING_BRANCH"
 		echo "Changing branchs from $WORKING_BRANCH to gh-pages"
 		git checkout gh-pages
-		echo "Merging changes from origin gh-pages"
-		git pull origin gh-pages
-		git commit -am "merging origin gh-pages"
-		echo "Pulling changes from $WORKING_BRANCH into gh-pages"
+		echo "Merging changes from origin main"
 		git pull origin "$WORKING_BRANCH"
-		echo "Merging changes from $WORKING_BRANCH"
+		git commit -am "Merge from $WORKING_BRANCH to gh-pages"
 		echo "Building website in gh-pages branch"
-		make installer.sh
+		make installer.sh man
 		make -f website.mak
-		git commit -am "merging $WORKING_BRANCH with gh-pages"
-		echo "Pushing changes up and publishing"
+		echo "Commiting changes after website/installer.sh build"
+		git commit -am "Merging site updates in gh-pages"
+		echo "Pushing changes up to gh-pages"
 		git push origin gh-pages
 		echo "Changing back to your working branch $WORKING_BRANCH"
 		git checkout "$WORKING_BRANCH"
