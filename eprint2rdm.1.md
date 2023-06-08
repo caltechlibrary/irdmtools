a6c4dcbc6c33e89e2d1ba98801fba839f9e4ed09
R. S. Doiel <rsdoiel@gmail.com>
2023-06-08 13:06:26 -0700

    Quick Save

--- eprint2rdm.1.md
+++ eprint2rdm.1.md
@@ -1,4 +1,4 @@
-%eprint2rdm(1) irdmtools user manual | version 0.0.17 8a944c7
+%eprint2rdm(1) irdmtools user manual | version 0.0.17 f8ec8ec
 % R. S. Doiel and Tom Morrell
 % 2023-06-08
 
--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2023-06-08"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "8a944c7"
+    ReleaseHash = "f8ec8ec"
 
     LicenseText = `
 Redistribution and use in source and binary forms, with or without
