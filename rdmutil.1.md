24fc0f7965e32cb270a49a805e35f01f1cff7dea
R. S. Doiel <rsdoiel@gmail.com>
2023-08-04 16:13:22 -0700

    Quick Save

--- rdmutil.1.md
+++ rdmutil.1.md
@@ -1,4 +1,4 @@
-%rdmutil(1) irdmtools user manual | version 0.0.29 15378a1
+%rdmutil(1) irdmtools user manual | version 0.0.29 f407538
 % R. S. Doiel and Tom Morrell
 % 2023-08-04
 
--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2023-08-04"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "15378a1"
+    ReleaseHash = "f407538"
 
     LicenseText = `
 Redistribution and use in source and binary forms, with or without
