0f43683d2f489b33372b1c653722f7380d735f38
R. S. Doiel <rsdoiel@gmail.com>
2023-08-15 09:44:27 -0700

    feat: improved status reporting eprints_to_rdm.py

--- get_eprintid_by_year.bash
+++ get_eprintid_by_year.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 YEAR="$1"
-mysql --batch --skip-column-names --execute "SELECT eprintid FROM eprint WHERE date_year = '$YEAR' AND eprint_status = 'archive' ORDER BY eprintid" "${REPO_ID}"
+mysql --batch --skip-column-names --execute "SELECT eprintid FROM eprint WHERE date_year = '$YEAR' AND eprint_status = 'archive' ORDER BY date_year, date_month, date_day, eprintid" "${REPO_ID}"
