90d03ae7b91b07c69f52a5aab12fa21eac1e8e77
R. S. Doiel <rsdoiel@gmail.com>
2023-08-25 13:33:00 -0700

    fix: improved mk_eprint_redirects.py to avoid dups, generates directly from psql if needed

--- migration/select_migrated_records.sql
+++ migration/select_migrated_records.sql
@@ -1,12 +1,11 @@
 --
--- I am trying to get a set of rows that let me get the eprintid, rdmid and record status
---
-SELECT json -> 'id' AS rdmid, json -> 'access' -> 'record' AS record_status, 
-       jsonb_array_elements(json -> 'metadata' -> 'identifiers') AS identifiers
-FROM rdm_records_metadata,
-       jsonb_array_elements(json -> 'metadata' -> 'identifiers') AS m
-WHERE 
-json -> 'access' ->> 'record' LIKE 'public'
-AND m @> '{ "scheme": "eprintid"}'
-ORDER BY RANDOM()
-LIMIT 10; 
+-- This SELECT takes the rdm_record_metadata table and generating a list of eprintid, rdmid and record status.
+-- filtered by those records which are "public".
+SELECT (t1.identifiers ->> 'identifier')::DECIMAL AS eprintid, t1.rdmid AS rdmid, t1.record_status AS record_status
+FROM (SELECT json ->> 'id' AS rdmid, json -> 'access' ->> 'record' AS record_status, 
+       		jsonb_array_elements(json -> 'metadata' -> 'identifiers') AS identifiers
+		FROM rdm_records_metadata
+		) AS t1 
+WHERE (t1.identifiers ->> 'scheme' LIKE 'eprintid')
+AND (t1.record_status LIKE 'public') 
+ORDER BY (t1.identifiers ->> 'identifier')::DECIMAL;
