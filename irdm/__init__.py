44491dfbfaba769f9291a2b7572d025356eef9bc
Tom Morrell <tmorrell@caltech.edu>
2024-01-08 15:41:14 -0800

    Cleanup

--- irdm/__init__.py
+++ irdm/__init__.py
@@ -1,14 +1,3 @@
-from .caltechdata_write import (
-    caltechdata_write,
-    write_files_rdm,
-    add_file_links,
-    send_to_community,
-)
-from .caltechdata_edit import (
-    caltechdata_edit,
-    caltechdata_unembargo,
-    caltechdata_accept,
-)
 from .customize_schema import customize_schema
 from .get_metadata import get_metadata
 from .download_file import download_file, download_url
