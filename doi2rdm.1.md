9eb40b17f5f9d570a005789f2300fecff4582c7b
R. S. Doiel <rsdoiel@gmail.com>
2024-01-16 17:21:06 -0800

    fix: official_url in rdm2eprint crosswalk

--- doi2rdm.1.md
+++ doi2rdm.1.md
@@ -1,4 +1,4 @@
-%doi2rdm(1) irdmtools user manual | version 0.0.68 0b325708
+%doi2rdm(1) irdmtools user manual | version 0.0.68 2425acab
 % R. S. Doiel and Tom Morrell
 % 2024-01-16
 
--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2024-01-16"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "0b325708"
+    ReleaseHash = "2425acab"
 
     LicenseText = `
 Redistribution and use in source and binary forms, with or without
