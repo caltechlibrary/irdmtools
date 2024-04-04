eba59b3f78aa74db2d196c7483361014497856dc
R. S. Doiel <rsdoiel@gmail.com>
2024-04-03 17:40:50 -0700

    added two arXiv doi to test datacite

--- doi2rdm_test.go
+++ doi2rdm_test.go
@@ -9,6 +9,8 @@ func TestDataCiteToRDM(t *testing.T) {
 	//FIXME: Need an arxiv DOI to look up at DataCite
 	arxiv_ids := []string{
 		"10.22002/D1.868",
+		"arXiv:2402.12335v1",
+		"arXiv:2401.12460v1",
 		/* these arXiv ids don't seem to have data at DataCite ...
 		"arXiv:2204.13532v2",
 		"arXiv:2312.07215",
@@ -21,31 +23,31 @@ func TestDataCiteToRDM(t *testing.T) {
 		"arXiv:2112.06016",
 		*/
 	}
-    app := new(Doi2Rdm)
-    app.Cfg = new(Config)
+	app := new(Doi2Rdm)
+	app.Cfg = new(Config)
 	for _, doi := range arxiv_ids {
 		options := new(Doi2RdmOptions)
 		options.MailTo = "dld-dev@library.caltech.edu"
 		obj, err := QueryDataCiteObject(app.Cfg, doi, options)
-        if err != nil {
-                t.Error(err)
-        }
+		if err != nil {
+			t.Error(err)
+		}
 		if obj == nil {
 			t.Errorf("expected a non-nil object for %q", doi)
 		}
 		record, err := CrosswalkDataCiteObject(app.Cfg, obj, options)
-        if err != nil {
-                t.Errorf("%s", err)
-				t.FailNow()
-        }
+		if err != nil {
+			t.Errorf("%s", err)
+			t.FailNow()
+		}
 		if record == nil {
 			t.Errorf("expected a non-nil record for doi %q", doi)
 			t.FailNow()
 		}
 		optionsFName, diffFName := "", ""
 		if err := app.RunDataCiteToRdm(os.Stdin, os.Stdout, os.Stderr, optionsFName, doi, diffFName); err != nil {
-             t.Error(err)
-			 t.FailNow()
-	    }
+			t.Error(err)
+			t.FailNow()
+		}
 	}
 }
